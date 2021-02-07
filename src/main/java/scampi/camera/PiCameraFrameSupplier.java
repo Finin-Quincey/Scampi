bb8d05b3adefe054d5723a28ee43879a59547c3a
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2021-02-07 18:45:06 +0000

    Remove JRPiCam stuff

--- src/main/java/scampi/camera/PiCameraFrameSupplier.java
+++ src/main/java/scampi/camera/PiCameraFrameSupplier.java
@@ -1,27 +1,21 @@
 package scampi.camera;
 
-import com.hopding.jrpicam.RPiCamera;
-import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
 import scampi.ScampiUI;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class PiCameraFrameSupplier implements FrameSupplier {
 
-	/** A reference to the {@link RPiCamera} object. */
-	private RPiCamera camera;
+
 
 	private final int width, height;
 
 	private BufferedImage frame;
 
-	public PiCameraFrameSupplier(int width, int height) throws FailedToRunRaspistillException {
+	public PiCameraFrameSupplier(int width, int height){
 
 		this.width = width;
 		this.height = height;
-
-		camera = new RPiCamera().setWidth(width).setHeight(height).turnOffPreview();
 
 		frame = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
@@ -30,11 +24,6 @@ public class PiCameraFrameSupplier implements FrameSupplier {
 	@Override
 	public BufferedImage getFrame(){
 
-		try {
-			frame = camera.takeBufferedStill();
-		}catch(IOException | InterruptedException e){
-			e.printStackTrace();
-		}
 
 		ScampiUI.logger.info(String.format("Frame: %s", frame));
 
